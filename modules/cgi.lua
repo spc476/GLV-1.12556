7bdd36d3bf14bb228ac2dfb22d5f5d28dc41b441
Sean Conner <spc@conman.org>
2019-07-31 03:07:10 -0400

    Pass along authorization to CGI scripts.
    You can either pass in native values (TLS_*) or Apache style (SSL_*) names
    to a CGI script.

--- modules/cgi.lua
+++ modules/cgi.lua
@@ -32,8 +32,10 @@ local nfl       = require "org.conman.nfl"
 local abnf      = require "org.conman.parsers.abnf"
 local lpeg      = require "lpeg"
 local io        = require "io"
+local os        = require "os"
 local string    = require "string"
 local coroutine = require "coroutine"
+local math      = require "math"
 local uurl      = require "url-util"
 
 local pairs     = pairs
@@ -101,6 +103,22 @@ end
 
 -- ************************************************************************
 
+local cert_parse do
+  local Cf = lpeg.Cf
+  local Cg = lpeg.Cg
+  local Ct = lpeg.Ct
+  local C  = lpeg.C
+  local P  = lpeg.P
+  local R  = lpeg.R
+  
+  local name   = R("AZ","az")^1
+  local value  = R(" .","0\255")^1
+  local record = Cg(P"/" * C(name) * P"=" * C(value))
+  cert_parse   = Cf(Ct"" * record^1,function(acc,n,v) acc[n] = v return acc end)
+end
+
+-- ************************************************************************
+
 local function fdtoios(fd)
   local newfd   = ios()
   newfd.__fd    = fd
@@ -142,7 +160,7 @@ end
 
 -- ************************************************************************
 
-return function(remote,program,location,conf)
+return function(ssl,remote,program,location,conf)
   local pipe = fsys.pipe()
   if not pipe then
     return 500,"Internal Error",""
@@ -194,6 +212,7 @@ return function(remote,program,location,conf)
       pipe.read:close()
     end
     
+    syslog('debug',"cwd=%q program=%q",fsys.getcwd(),tostring(program))
     local prog = uurl.rm_dot_segs:match(fsys.getcwd() .. "/" .. program)
     local args = parse_cgi_args:match(location.query or "") or {}
     local env  =
@@ -229,6 +248,8 @@ return function(remote,program,location,conf)
       env.PATH_TRANSLATED = fsys.getcwd() .. env.PATH_INFO
     end
     
+    -- ===================================================
+    
     local function add_http()
       env.REQUEST_METHOD       = "GET"
       env.SERVER_PROTOCOL      = "HTTP/1.0"
@@ -240,6 +261,8 @@ return function(remote,program,location,conf)
       env.HTTP_USER_AGENT      = ""
     end
     
+    -- ===================================================
+    
     local function add_apache()
       env.DOCUMENT_ROOT         = fsys.getcwd()
       env.CONTEXT_DOCUMENT_ROOT = env.DOCUMENT_ROOT
@@ -247,16 +270,74 @@ return function(remote,program,location,conf)
       env.SCRIPT_FILENAME       = prog
     end
     
+    -- ===================================================
+    
+    local function add_tlsenv(apache)
+      local function breakdown(base,fields)
+        for name,value in pairs(fields) do
+          env[base .. name] = value
+        end
+      end
+      
+      if not ssl:peer_cert_provided() then return end
+      
+      local notbefore = ssl:peer_cert_notbefore()
+      local notafter  = ssl:peer_cert_notafter()
+      local now       = os.time()
+      local remain    = tostring(math.floor(os.difftime(notafter,now) / 86400))
+      local I         = ssl:peer_cert_issuer()
+      local S         = ssl:peer_cert_subject()
+      local issuer    = cert_parse:match(I)
+      local subject   = cert_parse:match(S)
+      
+      if not apache then
+        env.TLS_CIPHER            = ssl:conn_cipher()
+        env.TLS_VERSION           = ssl:conn_version()
+        env.TLS_CLIENT_HASH       = ssl:peer_cert_hash()
+        env.TLS_CLIENT_ISSUER     = I
+        env.TLS_CLIENT_SUBJECT    = S
+        env.TLS_CLIENT_NOT_BEFORE = os.date("%Y-%m-%dT%H:%M:%SZ",notbefore)
+        env.TLS_CLIENT_NOT_AFTER  = os.date("%Y-%m-%dT%H:%M:%SZ",notafter)
+        env.TLS_CLIENT_REMAIN     = remain
+        
+        breakdown("TLS_CLIENT_ISSUER_", issuer)
+        breakdown("TLS_CLIENT_SUBJECT_",subject)
+        
+        env.AUTH_TYPE   = 'Certificate'
+        env.REMOTE_USER = env.TLS_CLIENT_SUBJECT_CN
+      else
+        env.SSL_CIPHER          = ssl:conn_cipher()
+        env.SSL_PROTOCOL        = ssl:conn_version()
+        env.SSL_CLIENT_I_DN     = I
+        env.SSL_CLIENT_S_DN     = S
+        env.SSL_CLIENT_V_START  = os.date("%b %d %H:%M:%S %Y GMT",notbefore)
+        env.SSL_CLIENT_V_END    = os.date("%b %d %H:%M:%S %Y GMT",notafter)
+        env.SSL_CLIENT_V_REMAIN = remain
+        env.SSL_TLS_SNI         = env.SERVER_NAME
+        
+        breakdown("SSL_CLIENT_I_DN_",issuer)
+        breakdown("SSL_CLIENT_S_DN_",subject)
+        
+        env.AUTH_TYPE   = 'Certificate'
+        env.REMOTE_USER = env.SSL_CLIENT_S_DN_CN
+      end
+    end
+    
+    -- ===================================================
+    
     local cwd = conf.cwd
     
-    if conf.http     then add_http()   end
-    if conf.apache   then add_apache() end
+    if conf.http     then add_http()              end
+    if conf.apache   then add_apache()            end
+    if conf.envtls   then add_tlsenv(info.apache) end
+    
     if conf.instance then
       for name,info in pairs(conf.instance) do
         if location.path:match(name) then
-          if info.cwd    then cwd = info.cwd end
-          if info.http   then add_http()     end
-          if info.apache then add_apache()   end
+          if info.cwd     then cwd = info.cwd            end
+          if info.http    then add_http()                end
+          if info.apache  then add_apache()              end
+          if info.envtls  then add_tlsenv(info.apache)   end
           if info.env then
             for var,val in pairs(info.env) do
               env[var] = val
