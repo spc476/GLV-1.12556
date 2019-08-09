89e93acdf09d62fc93bfe988dcee42b0426408f4
Sean Conner <spc@conman.org>
2019-08-08 20:30:48 -0400

    Gemini officially has two-digit status codes!
    Fix the client first.  Until all the servers update, support the earlier
    standard status codes.

--- Lua/client.lua
+++ Lua/client.lua
@@ -42,87 +42,18 @@ local statparse do
   local P  = lpeg.P
   local R  = lpeg.R
   
-  local DD       = R"09" * R"09"
-  local status   = P"200"    * Cc'okay'     * Cc'content'
-                 + P"301"    * Cc'redirect' * Cc'permanent'
-                 + P"401"    * Cc'client'   * Cc'unauthorized'
-                 + P"403"    * Cc'client'   * Cc'forbidden'
-                 + P"404"    * Cc'client'   * Cc'not-found'
-                 + P"410"    * Cc'client'   * Cc'gone'
-                 + P"429"    * Cc'client'   * Cc'slow-down'
-                 + P"460"    * Cc'client'   * Cc'need-certificate'
-                 + P"461"    * Cc'client'   * Cc'future-certificate'
-                 + P"462"    * Cc'client'   * Cc'expired-certificate'
-                 + P"463"    * Cc'client'   * Cc'rejected-certificate'
-                 
-                 -- ----------------
-                 -- Catch-all path
-                 -- ----------------
-                 
-                 + P"2" * DD * Cc'okay'     * Cc'content'
-                 + P"3" * DD * Cc'redirect' * Cc'see-other'
-                 + P"4" * DD * Cc'client'   * Cc'error'
-                 + P"5" * DD * Cc'server'   * Cc'error'
-                 
-                 -- ----------------------------------------------------
-                 -- Proposed 2-digit codes---The main categories aren't
-                 -- quite right, bit they're the closest to the current
-                 -- categories I'm using now.
-                 -- ----------------------------------------------------
-                 
-                 + P"10" * Cc'search'   * Cc'need-info'
-                 + P"20" * Cc'okay'     * Cc'content'
-                 + P"30" * Cc'redirect' * Cc'temporary'
-                 + P"31" * Cc'redirect' * Cc'permanent'
-                 + P"40" * Cc'server'   * Cc'error'
-                 + P"41" * Cc'server'   * Cc'overload'
-                 + P"42" * Cc'server'   * Cc'CGI-error'
-                 + P"43" * Cc'client'   * Cc'slow-down'
-                 + P"50" * Cc'server'   * Cc'error'
-                 + P"51" * Cc'client'   * Cc'not-found'
-                 + P"53" * Cc'client'   * Cc'gone'
-                 + P"59" * Cc'client'   * Cc'bad-request'
-                 + P"60" * Cc'client'   * Cc'need-certificate'
-                 + P"61" * Cc'client'   * Cc'need-certificate'
-                 + P"62" * Cc'client'   * Cc'need-certificate'
-                 + P"63" * Cc'client'   * Cc'rejected-certificate'
-                 + P"64" * Cc'client'   * Cc'future-certificate'
-                 + P"65" * Cc'client'   * Cc'expired-certificate'
-                 
-                 -- -----------------------------
-                 -- The proposed catch-all path
-                 -- -----------------------------
-                 
-                 + P"1" * R"09" * Cc'search'   * Cc'need-info'
+  local status   = P"1" * R"09" * Cc'input'    * Cc'required'
                  + P"2" * R"09" * Cc'okay'     * Cc'content'
                  + P"3" * R"09" * Cc'redirect' * Cc'temporary'
-                 + P"4" * R"09" * Cc'server'   * Cc'error'
-                 + P"5" * R"09" * Cc'client'   * Cc'error'
-                 + P"6" * R"09" * Cc'server'   * Cc'need-certificate'
+                 + P"4" * R"09" * Cc'error'    * Cc'temporary'
+                 + P"5" * R"09" * Cc'error'    * Cc'permanent'
+                 + P"6" * R"09" * Cc'auth'     * Cc'required'
                  
-                 -- -----------------------------------
-                 -- The "Official Spec" return codes.
-                 -- -----------------------------------
-                 
-                 + P"2"      * Cc'okay'     * Cc'content'
-                 + P"3"      * Cc'redirect' * Cc'permanent'
-                 + P"4"      * Cc'client'   * Cc'error'
-                 + P"5"      * Cc'server'   * Cc'error'
-                 + P"9"      * Cc'client'   * Cc'slow-down'
-                 
-                 -- ---------------------------------------------------
-                 -- The proposed 1-dight status codes---Most of these
-                 -- match the existing structure and only add to them.
-                 -- ---------------------------------------------------
-                 
-                 + P"1"      * Cc'search' * Cc'need-info'
-                 + P"6"      * Cc'client' * Cc'gone'
-                 + P"7"      * Cc'client' * Cc'slow-down'
-                 + P"A"      * Cc'client' * Cc'need-certificate'
-                 + P"B"      * Cc'client' * Cc'need-certificate'
-                 + P"C"      * Cc'client' * Cc'rejected-certificate'
-                 + P"D"      * Cc'cilent' * Cc'expired-certificate'
-                 
+                 + P"2"         * Cc'okay'     * Cc'content'
+                 + P"3"         * Cc'redirect' * Cc'permanent'
+                 + P"4"         * Cc'error'    * Cc'permanent'
+                 + P"5"         * Cc'error'    * Cc'temporary'
+                 + P"9"         * Cc'error'    * Cc'slow-down'
   local infotype = P"\t" * C(R" \255"^0)
                  + Cc"type/text; charset=utf-8"
                  
@@ -188,8 +119,8 @@ local function main(location,usecert)
         info
   ))
   
-  if system == 'client' then
-    if status == 'need-certificate' and CERT and KEY then
+  if system == 'auth' then
+    if status == 'required' and CERT and KEY then
       return main(location,true)
     end
   end
