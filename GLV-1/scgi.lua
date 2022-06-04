ffdce87de3ccd4b96c139a91a975cf6ae8ac6d50
Sean Conner <spc@conman.org>
2022-06-04 02:53:56 -0400

    Feature---optionally start up SCGI programs
    The path segment of the scgi: URL is split into two sections (using a
    comma):
    
    	scgi://example.com:1234/
    		The script needs to be running at the given network address
    
    	scgi://example.com:1234/,/path/to/script
    		GLV-1.12556 will start /path/to/script upon first access
    		(aka socket activation) and monitor it, restarting it if
    		required.
    
    	scgi:/path/to/unix-socket
    		The script needs to be running, listening in on the
    		other end of the named UNIX socket.
    
    	scgi:/path/to/unix-socket,/path/to/script
    		GLV-1.12556 will start /path/to/script upon first access
    		(aka socket activation) and monitor it, restarting it if
    		required.

--- GLV-1/scgi.lua
+++ GLV-1/scgi.lua
@@ -21,16 +21,36 @@
 -- ************************************************************************
 -- luacheck: ignore 611
 
-local syslog = require "org.conman.syslog"
-local fsys   = require "org.conman.fsys"
-local errno  = require "org.conman.errno"
-local url    = require "org.conman.parsers.url"
-local net    = require "org.conman.net"
-local tcp    = require "org.conman.nfl.tcp"
-local gi     = require "GLV-1.gateway"
+local syslog    = require "org.conman.syslog"
+local fsys      = require "org.conman.fsys"
+local process   = require "org.conman.process"
+local signal    = require "org.conman.signal"
+local errno     = require "org.conman.errno"
+local url       = require "org.conman.parsers.url"
+local net       = require "org.conman.net"
+local nfl       = require "org.conman.nfl"
+local tcp       = require "org.conman.nfl.tcp"
+local gi        = require "GLV-1.gateway"
+local coroutine = require "coroutine"
 
-local pairs    = pairs
-local tostring = tostring
+local pairs     = pairs
+local tostring  = tostring
+
+local PID = {}
+
+-- ************************************************************************
+
+signal.catch('child',function()
+  local info,err = process.wait()
+  if not info then
+    syslog('error',"wait() = %q",errno[err])
+  else
+    local path = PID[info.pid]
+    PID[info.pid] = nil
+    PID[path]     = nil
+    syslog('warning',"%s: pid=%d status=%s description=%s",path,info.pid,info.status,info.description)
+  end
+end)
 
 -- ************************************************************************
 
@@ -81,6 +101,31 @@ return function(auth,program,directory,base,location,ios)
   end
   
   local addr
+  local tsock,path = scgiloc.path:match("^([^,]*),?(.*)")
+  
+  if path ~= "" then
+    if not PID[path] then
+      local pid,err1 = process.fork()
+      if not pid then
+        syslog('error',"fork() = %s",errno[err1])
+        ios:write("40\r\n")
+        return 40
+      end
+      if pid == 0 then
+        signal.default('int')
+        signal.default('term')
+        signal.default('child')
+        process.exec(path,{})
+        process.exit(127)
+      else
+        syslog('info',"starting SCGI %s",path)
+        PID[path] = pid
+        PID[pid]  = path
+        nfl.timeout(.1)
+        coroutine.yield()
+      end
+    end
+  end
   
   if scgiloc.host then
     if not scgiloc.port then
@@ -91,13 +136,13 @@ return function(auth,program,directory,base,location,ios)
     
     addr = net.address2(scgiloc.host,'any','tcp',scgiloc.port)[1]
   else
-    if scgiloc.path == "" then
+    if tsock == "" then
       syslog('error',"SCGI: %q missing path",scgiurl)
       ios:write("40\r\n")
       return 40
     end
     
-    addr = net.address(scgiloc.path,'tcp')
+    addr = net.address(tsock,'tcp')
   end
   
   local inp = tcp.connecta(addr,5)
